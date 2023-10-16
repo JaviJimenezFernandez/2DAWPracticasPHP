0898e3cc1f6bc2df510e25bc053c40eca381e47c
JavierJimenez <superjevi@hotmail.com>
2023-10-16 18:01:55 +0200

    Añadido otro archivo PHP como prueba de la UD2

---
+++ Tarea2GitHub/hola.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Primer php</title>
+  </head>
+  <body>
+
+    <h1>Este es un párrafo HTML</h1>
+    <?php
+        echo "Hola Javi, esto es código PHP";
+    ?>
+  </body>
+</html>
--- Tarea2GitHub/index.php
+++ Tarea2GitHub/index.php
@@ -1,3 +1,14 @@
-<?php
-echo 'Tarea 2 GitHub';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primer PHP</title>
+</head>
+<body>
+    <h1>
+    <?php echo 'Tarea 2 GitHub' ?>
+    </h1>
+</body>
+</html>
+
